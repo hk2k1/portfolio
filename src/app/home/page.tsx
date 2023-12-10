8722cc24f0f0f023e7aa050d2faeafc8b4b0ec32
hk2k1 <m.harshakeerthan@gmail.com>
2023-12-11 00:09:42 +0800

    Portfolio Front Page added with animations

---
+++ src/app/home/page.tsx
@@ -0,0 +1,21 @@
+import './home.scss'
+
+const Home = () => {
+    return ( 
+        <div className="container-main">
+            <div className='container-grain'>
+                <div className='container-text'>Home</div>
+                <p>
+                    Hello world, I am
+                </p>
+                <h1>
+                    Harsha
+                </h1>
+                <button className="btn btn-primary" />
+                <div className=""></div>
+            </div>    
+        </div>
+     );
+}
+ 
+export default Home;
---
+++ src/app/layout.tsx
@@ -0,0 +1,70 @@
+import type { Metadata } from 'next'
+import { Inter } from 'next/font/google'
+import localFont from 'next/font/local'
+
+// const inter = Inter({ subsets: ['latin'] })
+// const gambetta = localFont({ src: '../styles/fonts/WEB/fonts'})
+const gambetta = localFont({
+  src: [
+    {
+      path: '../styles/Gambetta_Complete/Fonts/WEB/fonts/Gambetta-Regular.woff2',
+      weight: '400',
+      style: 'regular',
+    },
+    {
+      path: '../styles/Gambetta_Complete/Fonts/WEB/fonts/Gambetta-Bold.woff2',
+      style: 'bold',
+    },
+    // {
+    //   path: '../styles/Gambetta_Complete/Fonts/WEB/fonts/Gambetta-BoldItalic.woff2',
+    //   style: 'boldItalic',
+    // },
+    {
+      path: '../styles/Gambetta_Complete/Fonts/WEB/fonts/Gambetta-Italic.woff2',
+      style: 'italic',
+    },
+    // {
+    //   path: '../styles/Gambetta_Complete/Fonts/WEB/fonts/Gambetta-Light.woff2',
+    //   style: 'light',
+    // },
+    // {
+    //   path: '../styles/Gambetta_Complete/Fonts/WEB/fonts/Gambetta-LightItalic.woff2',
+    //   style: 'LightItalic',
+    // },
+    // {
+    //   path: '../styles/Gambetta_Complete/Fonts/WEB/fonts/Gambetta-MediumItalic.woff2',
+    //   style: 'MediumItalic',
+    // },
+    // {
+    //   path: '../styles/Gambetta_Complete/Fonts/WEB/fonts/Gambetta-SemiboldItalic.woff2',
+    //   style: 'SemiboldItalic',
+    // },
+    {
+      path:'../styles/Gambetta_Complete/Fonts/WEB/fonts/Gambetta-Variable.woff2',
+      style: 'variable',
+    },
+    // {
+    //   path: '../styles/Gambetta_Complete/Fonts/WEB/fonts/Gambetta-VariableItalic.woff2',
+    //   style: 'VariableItalic'
+    // }
+  ]
+})
+
+import './globals.css'
+
+export const metadata: Metadata = {
+  title: 'Harsha Portfolio',
+  description: 'Personal Website of a university student',
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en">
+      <body className={gambetta.className}>{children}</body>
+    </html>
+  )
+}
