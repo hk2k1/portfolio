5a3b28a9c5f0f3ee293e8a744c085a3895743081
hk2k1 <m.harshakeerthan@gmail.com>
2024-05-05 03:21:53 +0800

    Grainy Textures tested

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,17 +7,32 @@ import "../styles/css/app.scss";
 import HelloName from "@/components/HelloName";
 
 import animation from "../styles/css/animation.module.scss";
+import Grainy from "@/components/Grainy";
 
 export default function Home() {
   return (
     <main>
       <div className="container-main">
-        <div className="container-grain">
-          <div className="components">
-            <HelloName />
+        <div className="wrapper-grain">
+          <div className="container-grain">
+            <div className="components">
+              <HelloName />
+            </div>
           </div>
         </div>
       </div>
+      {/* <svg viewBox="0 0 243 243">
+        <filter id="noiseFilter">
+          <feTurbulence
+            type="franctalNoise"
+            baseFrequency="2.3"
+            numOctaves="1"
+            result="turbulence"
+          />
+        </filter>
+        <rect width="100%" height="100%" fill="url(#noiseFilter)" />
+      </svg> */}
+      {/* <Grainy /> */}
     </main>
   );
 }
