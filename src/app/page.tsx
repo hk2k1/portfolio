22a27a3d29b29d68410cfab86141bec92cda9fb7
hk2k1 <m.harshakeerthan@gmail.com>
2024-05-04 22:55:24 +0800

    Name updated into its own component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,63 +1,23 @@
-'use client';
+"use client";
 
-import Image from 'next/image'
-import { useEffect, useRef } from 'react'
-import '../styles/css/app.scss'
+import Image from "next/image";
+import { useEffect, useRef } from "react";
+import "../styles/css/app.scss";
 
-import animation from '../styles/css/animation.module.scss'
+import HelloName from "@/components/HelloName";
 
+import animation from "../styles/css/animation.module.scss";
 
 export default function Home() {
-  const helloWorldRef = useRef<HTMLDivElement>(null);
-  const helloNameRef = useRef<HTMLHeadingElement>(null);
-
-  useEffect(() => {
-    const helloWorld = helloWorldRef.current;
-    const helloName = helloNameRef.current;
-
-    const onHelloWorldAnimationEnd = () => {
-      if (helloName){
-        helloWorld?.removeEventListener('animationend', onHelloWorldAnimationEnd);
-        animateText(helloName);
-      }
-    }
-    if(helloWorld) {
-      helloWorld.classList.add(animation.animated);
-      helloWorld.addEventListener('animationend', () => {
-        if(helloName){
-          helloName.classList.add(animation.animated);
-          animateText(helloName);
-        }
-      });
-    }
-  }, []);
-
-  const animateText = (element: HTMLElement) => {
-    const text = element.textContent;
-    element.textContent = '';
-    let i = 0;
-    const interval = setInterval(() => {
-      if( i < text!.length ){
-        element.textContent += text![i];
-        i++;
-      } else {
-        clearInterval(interval);
-      }
-    }, 100);
-  };
-
   return (
-    <main >
-      <div className='container-main'>
-        <div className='container-grain'>
-          <div className='components'>
-          <div ref={helloWorldRef} className='hello-world'>
-            Hello World,
-          </div>
-          <h1 ref={helloNameRef} className='hello-name'>I am Harsha</h1>
+    <main>
+      <div className="container-main">
+        <div className="container-grain">
+          <div className="components">
+            <HelloName />
           </div>
         </div>
       </div>
     </main>
-  )
+  );
 }
---
+++ src/components/HelloName/index.tsx
@@ -0,0 +1,58 @@
+"use client";
+import React, { useEffect, useRef } from "react";
+import animation from "../../styles/css/animation.module.scss";
+import "../../styles/css/app.scss";
+
+export default function HelloName() {
+  const helloWorldRef = useRef<HTMLDivElement>(null);
+  const helloNameRef = useRef<HTMLHeadingElement>(null);
+
+  useEffect(() => {
+    const helloWorld = helloWorldRef.current;
+    const helloName = helloNameRef.current;
+
+    const onHelloWorldAnimationEnd = () => {
+      if (helloName) {
+        helloWorld?.removeEventListener(
+          "animationend",
+          onHelloWorldAnimationEnd
+        );
+        animateText(helloName);
+      }
+    };
+    if (helloWorld) {
+      helloWorld.classList.add(animation.animated);
+      helloWorld.addEventListener("animationend", () => {
+        if (helloName) {
+          helloName.classList.add(animation.animated);
+          animateText(helloName);
+        }
+      });
+    }
+  }, []);
+
+  const animateText = (element: HTMLElement) => {
+    const text = element.textContent;
+    element.textContent = "";
+    let i = 0;
+    const interval = setInterval(() => {
+      if (i < text!.length) {
+        element.textContent += text![i];
+        i++;
+      } else {
+        clearInterval(interval);
+      }
+    }, 100);
+  };
+
+  return (
+    <div>
+      <div ref={helloWorldRef} className="hello-world">
+        Hello World,
+      </div>
+      <h1 ref={helloNameRef} className="hello-name">
+        I am Harsha
+      </h1>
+    </div>
+  );
+}
