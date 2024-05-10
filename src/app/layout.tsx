7344ae241096379ff61594da342ae3cd49f79391
hk2k1 <86213550+hk2k1@users.noreply.github.com>
2024-05-10 20:02:54 +0800

    Added meta tag

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -78,6 +78,12 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" className="!scroll-smooth">
+      <head>
+        <meta
+          name="google-site-verification"
+          content="y1zdwNcIWZ-yQqKtmaGVLIyPriuVcOfFmSatPLquzU0"
+        />
+      </head>
       <body
         className={`${gambetta.className} bg-gray-50 text-gray-950 relative pt-28 sm:pt-36 dark:bg-gray-900 dark:text-gray-50 dark:text-opacity-90`}
       >
